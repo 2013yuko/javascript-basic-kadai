cfb58d4fb3bf4e901e7e056bda1fe1998fc87aee
2013yuko <2013yuko1005@gmail.com>
2023-11-26 15:12:14 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,9 @@
 let bodyWeight;
 let bodyHight;
+let BMI;
 
-bodyWeight = '68';
-bodyHight = '1.7';
+bodyWeight = 68;
+bodyHight = 1.7;
+BMI = bodyWeight/(bodyHight*bodyHight);
 
-console.log(bodyWeight/(bodyHight*bodyHight));
+console.log(BMI);

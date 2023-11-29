54c060864033f53e0ed5e97c42406688d27439c2
2013yuko <2013yuko1005@gmail.com>
2023-11-29 19:28:35 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,6 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
-const text2 = document.getElementById('text2')
 
 btn.addEventListener('click', () => {
-    text.style.display = 'none';
-    text2.style.display = 'block';
+    text.textContent = 'ボタンをクリックしました';
 });
